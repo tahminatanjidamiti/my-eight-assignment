d08a0a75dc6d3752bc3631ce3b70791f2b99a9bb
tahminatanjidamiti <mity2027@gmail.com>
2024-11-10 01:05:06 +0600

    added .json file and modified code

--- src/Components/AllProduct/AllProduct.jsx
+++ src/Components/AllProduct/AllProduct.jsx
@@ -1,5 +1,5 @@
-import { NavLink, Outlet, useLoaderData } from 'react-router-dom';
-import Product from '../Product/Product';
+import { NavLink, Outlet } from 'react-router-dom';
+
 
 
 const AllProduct = () => {
